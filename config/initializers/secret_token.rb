aaafc86206b4a1420d6560d79a4f019c2d2fcf4d
Godfrey Mlowe <mlowegodfrey@gmail.com>
2014-10-25 07:35:27 +0000

    $ rails new godfreym-forum

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GodfreymForum::Application.config.secret_key_base = 'da6a1dfa7ec5f0ba737ca50c351a77f37adf275cc3ffff9f1c5b43eda266d95db839d710514902e0438bd06df7b28bcacb9e3a72aa5a85cfcad1b9f334db5111'
